c9c44e0e80bcca83cf545148f69c80c336f47950
Sanchit Sahay <sahaysanchit14@gmail.com>
2020-03-27 19:08:19 +0530

    Starter Code for Task-1

---
+++ task-1/app.js
@@ -0,0 +1,42 @@
+//Importing the package 'express', and then initialising it in the variable 'app' which let's you handle requests
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+var items = [
+    //... Enter some test items here
+    'hello world'
+];
+
+//Serve a GET Request for url '/all' and '/:id'
+//The arrow function here controls what gets sent when the request is made
+app.get('/all', (req, res) => res.send(items));
+
+
+// 'id' here acts as a URL parameter
+// Another way to handle the function is through unnamed functions as shown below
+app.get('/item/:id', function(req, res){
+    //Implement searching for ID and send
+    res.send(`Searched for ${req.params["id"]}`);
+});
+
+
+//Serve POST Requests
+
+//You can also define a separate function and call that later, as long as the function parameters match
+function addItem(req, res){
+    //Implement adding item here
+    res.send("Added Item");
+}
+
+app.post('/additem', addItem);
+
+function editItem(req, res){
+
+}
+
+app.put('/edititem/:id', editItem);
+
+//This is what runs your backend server on localhost:portnumber the portnumber can be anything, the callback arrow function notifies you about the server being up
+app.listen(port, () => console.log(`Listening at port ${port}`));
