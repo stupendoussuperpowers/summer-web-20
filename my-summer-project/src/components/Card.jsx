cf5f8db7045452f27f98031c95c0876f4152fd70
Rhea-Adhikari <rheadhikari@gmail.com>
2020-04-20 08:40:40 +0530

    pause/play button

--- my-summer-project/src/components/Card.jsx
+++ my-summer-project/src/components/Card.jsx
@@ -3,28 +3,36 @@ import React ,{useState}from "react";
 
 function Card(props){
 
-   const [value,setValue]=useState(false)
+   const [value,setValue]=useState(null)
    
 
-   function isClicked(){
+   function playClicked(){
        setValue(true);
        
    }
-   function isdoubleClicked(){
+   function pauseClicked(){
     setValue(false);
     
 }
-   
+
+ 
 
 
-return <button  onClick ={isClicked} 
-onDoubleClick={isdoubleClicked}
-className={(value===true) ? "cardclicked" :"card"}>
+return <div className="card">
 
 <h1 className="song">{props.id}) {props.name}</h1>
 <h3 className="artist">{props.artist}</h3>
+<button onClick ={playClicked} 
 
+className={(value===true) ? "cardclicked" :"card"}>
+Play
 </button>
+<button onClick ={pauseClicked} 
+
+className={(value===false) ? "cardclicked" :"card"}>
+Pause
+</button>
+</div>
 }
 
 export default Card;
