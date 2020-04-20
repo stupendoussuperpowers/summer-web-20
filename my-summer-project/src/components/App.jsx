b5274f9837c1f062f6bc5eee8a44d1a54425634e
Rhea-Adhikari <rheadhikari@gmail.com>
2020-04-20 08:34:32 +0530

    double click to pause single click to start

--- my-summer-project/src/components/App.jsx
+++ my-summer-project/src/components/App.jsx
@@ -3,12 +3,11 @@ import Card from "./Card";
 import songs from "../songs";
 import Head from "./Head"
 
-var userHasClicked=false;
+
 
 function createCard(item)
 {
     return <Card 
-    isClicked={userHasClicked}
     id={item.id}
     name={item.name}
     artist={item.artist}
--- my-summer-project/src/components/Card.jsx
+++ my-summer-project/src/components/Card.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React ,{useState}from "react";
 
 
 function Card(props){
 
+   const [value,setValue]=useState(false)
+   
+
+   function isClicked(){
+       setValue(true);
+       
+   }
+   function isdoubleClicked(){
+    setValue(false);
+    
+}
    
 
 
-return <button  className="card">
+return <button  onClick ={isClicked} 
+onDoubleClick={isdoubleClicked}
+className={(value===true) ? "cardclicked" :"card"}>
 
 <h1 className="song">{props.id}) {props.name}</h1>
 <h3 className="artist">{props.artist}</h3>
+
 </button>
 }
 
