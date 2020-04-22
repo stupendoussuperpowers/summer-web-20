c457d20d4403f774d7ef49d1f8983602f2ca3de5
Rhea-Adhikari <rheadhikari@gmail.com>
2020-04-22 17:22:20 +0530

    not able to access parent from child component -trying make play/pause toggle even on clicking a card

--- my-summer-project/src/components/App.jsx
+++ my-summer-project/src/components/App.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import Card from "./Card";
 import songs from "../songs";
 import Head from "./Head"
+
 class App extends Component{
 
     constructor() {
@@ -22,6 +23,7 @@ class App extends Component{
 render(){
     return <div>
         <Head />
+       
         <button className="myButton" onClick={this.toggleState}>{this.state.trueOrFalse ? "Play" : "Pause"}</button>
         {songs.map(
             function(item,i){
