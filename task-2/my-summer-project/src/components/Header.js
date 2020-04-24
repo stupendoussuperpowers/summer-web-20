f499d0ed5f80468efeeef29b27cec8409501b5fb
Dyutish <bdyutish@gmail.com>
2020-04-24 19:22:15 +0530

    Integrated api in task2

--- task-2/my-summer-project/src/components/Header.js
+++ task-2/my-summer-project/src/components/Header.js
@@ -5,16 +5,29 @@ export class Header extends React.Component{
     
     constructor(props) {
         super(props);
+        this.state={
+            albumtitle:""
+        }
        
       }
-
+    componentDidMount(){
+        fetch("https://musicbrainz.org/ws/2/release/9d6d1e4c-bc3b-4a8e-82bc-5ea5ef36bc02?inc=aliases+artist-credits+labels+discids+recordings&fmt=json")
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            // console.log(data.title);
+            this.setState({
+                albumtitle:data.title
+            })
+        })
+    }
     render()
     {
 
         return(
             <div className="album">
             <img className="album-cover" src="https://upload.wikimedia.org/wikipedia/en/c/c8/Green_Day_-_21st_Century_Breakdown_cover.jpg" alt ="album pic"></img>
-            <h1 className="album-title">{this.props.albumtitle}</h1>
+            <h1 className="album-title">{this.state.albumtitle}</h1>
 
             </div>
         )
--- task-2/my-summer-project/src/components/Songlist.js
+++ task-2/my-summer-project/src/components/Songlist.js
@@ -5,7 +5,8 @@ export class Songlist extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            nowplaying: "-1"
+            nowplaying: "-1",
+            tracks: []
         }
         this.click = this.click.bind(this);
         }
@@ -14,21 +15,51 @@ export class Songlist extends React.Component{
             this.setState({nowplaying:id});
             console.log(this.state);
       }
+    todur = function (sec_num) {
+        // var sec_num = parseInt(this, 10); // don't forget the second param
+        sec_num = sec_num/1000;
+        var hours   = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = Math.floor(sec_num - (hours * 3600) - (minutes * 60));
+    
+        if (hours   < 10) {hours   = "0"+hours;}
+        if (minutes < 10) {minutes = "0"+minutes;}
+        if (seconds < 10) {seconds = "0"+seconds;}
+        return minutes+':'+seconds;
+    }
+      componentDidMount(){
+        fetch("https://musicbrainz.org/ws/2/release/9d6d1e4c-bc3b-4a8e-82bc-5ea5ef36bc02?inc=aliases+artist-credits+labels+discids+recordings&fmt=json")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.media[0].tracks);
+            // console.log(data.title);
+            this.setState({
+                tracks : data.media[0].tracks
+            })
+        })
+    }
     
     render(){
         return(
             <div>
             <div className="song-list">
-                <Song id="1" title="21 Guns" action={() =>this.click("1")} np={this.state.nowplaying} dur="4:00"  />
-                <Song id="2" title="Song of the Century" action={() =>this.click("2")} np={this.state.nowplaying} dur="4:00"/>
-                <Song id="3" title="Know Your Enemy" action={() =>this.click("3")} np={this.state.nowplaying} dur="4:00"/>
-                <Song id="4" title="See the Light" action={() =>this.click("4")} np={this.state.nowplaying} dur="4:00"/>
-                <Song id="5" title="Relentless Heart Syndrome" action={() =>this.click("5")} np={this.state.nowplaying} dur="4:00"/>
+                {this.state.tracks.map((val,ind) => (
+                    
+                    <Song id={this.state.tracks[ind].position} title={this.state.tracks[ind].title} action={() =>this.click(ind+1)} np={this.state.nowplaying} dur={this.todur(this.state.tracks[ind].length)}  />
+                ))}
+                
             </div>
             <NowPlaying np={this.state.nowplaying}/>
             </div>
-        )
+        );
     }
 }
 
 export default Songlist;
+
+// Here are the details of your new API account.
+
+// Application name	.
+// API key	975b099fafe0b9bf50c415cd68b52c2d
+// Shared secret	5974fb8e2d2926c9b7efc16dde69ac4e
+// Registered to	bdyutish
